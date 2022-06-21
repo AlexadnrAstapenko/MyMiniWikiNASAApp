717369db3b501df7e8190a0378ec9649cc705696
AlexadnrAstapenko <86770567+AlexadnrAstapenko@users.noreply.github.com>
2022-06-21 10:42:42 +0300

    Add hyperLin on about app, add animation on login/registration

--- NASAWiKi/NASAWiKi/Controllers/About/AboutApp.swift
+++ NASAWiKi/NASAWiKi/Controllers/About/AboutApp.swift
@@ -8,11 +8,37 @@
 import Foundation
 import UIKit
 
-class AboutApp: UIViewController {
+class AboutApp: UIViewController, UITextViewDelegate {
+    // MARK: Internal
+
+    @IBOutlet var aboutTextTView: UITextView!
     
-    @IBOutlet weak var aboutTextTView: UITextView!
+    var URL = "https://api.nasa.gov"
+
+    var aboutApp = "This application was created to study photographs from NASA. It uses a trial version of the NASA API to view information and photos from space and beyond. The interface of the application itself and additional information will be presented below. This information was taken from here"
     
+    override func viewDidLoad() {
+        setUpUI()
+        aboutTextTView.delegate = self
+    }
+
+    func textView(_: UITextView, shouldInteractWith URL: URL, in _: NSRange, interaction _: UITextItemInteraction) -> Bool {
+        UIApplication.shared.open(URL)
+        return false
+    }
+
     @IBAction func backBtn(_: Any) {
         selectVC(vc: MainPageVC(), storyboard: .main)
+    }
+
+    // MARK: Private
+
+    private func setUpUI() {
+        let attributedString = NSMutableAttributedString(string: aboutApp)
+        attributedString.addAttribute(.link,
+                                      value: "https://api.nasa.gov",
+                                      range: NSRange(location: 278, length: 5))
+        aboutTextTView.attributedText = attributedString
+        aboutTextTView.font = UIFont(name: "Helvetica Oblique", size: 27)
     }
 }
